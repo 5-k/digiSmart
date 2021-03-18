c1c18bf821e432a10062b2941e68702817489406
Prateek Mishra <prateek15mishra@gmail.com>
2021-03-19 02:58:04 +0530

    Update maindataproces.py

--- maindataproces.py
+++ maindataproces.py
@@ -13,8 +13,8 @@ import math
 import os
 import boto3 
 import collections
-from datetime import datetime
- 
+from datetime import datetime 
+import botocore
 
 
 class SimpleJsonStruct:
@@ -34,42 +34,50 @@ class DataJsonFlatten:
     #There are the prefixes of valid sources we want to read from input json. 
     #validCrossWalks= ["CODS"]
     validCrossWalks=["CODS","CTLN","MDU","MCRM","EMBS","DATAVISION","CMS","PBMD"]
-    fileVal = None
-    outLogfile = None
-    dictionary_SimpleModels = {}
-
+    
+    dictionary_SimpleModels = {} 
     output_data = [] 
     logData = []
-    output_type='json'  
+
     #Required Fields in Output Json
     list_RequiredField_For_Single_Nested_Data=["HCPUniqueId","ActiveFlag","EffectiveStartDate","EffectiveEndDate","CountryCode","Name","FirstName","MiddleName","LastName",
     "Gender","Source","OriginalSource", "Address", "Specialities"]
-
     list_RequiredField_For_Double_Nested_Data=["RegulatoryAction","Email","Phone"]
     list_RequiredField_From_CrossWalk = ["CrossWalkUri", "CrossWalkValue","CreateDate"]
     list_RequiredField_Custom=["EntityId"]
 
     #Env Run Variables
     log_data_to_file = True
-    is_local_run = True
+    is_local_run = False
     print_to_console = True 
-    read_line_byLine = False
-    
+    read_line_byLine = True
+
+    #Updated RunTime Variables
     fileName = "" 
     POC_INBOUND = ""
-
     outputFilePath = "" 
     logFilePath = "" 
     outputFileName = "" 
     logFileName = ""
+    fileVal = None
+    outLogfile = None 
+    final_output_string_for_s3 = ''
+    final_log_string_for_s3 = ''
 
+    #static string
     fileConcatinator = '/' 
+    bucket_name = 'lly-future-state-arch-poc-dev'
+    output_type='json' 
 
     def __init__(self):
         print("Information: Initialized Script")
 
     def initDictionary(self): 
-        self.dictionary_SimpleModels = {}
+        self.dictionary_SimpleModels = {} 
+
+    def initVariables(self): 
+        self.initDictionary()
+        self.output_data = [] 
 
     def get_myDictionary(self, dictionary, key):
         return dictionary[key]
@@ -94,7 +102,7 @@ class DataJsonFlatten:
 
         if(self.is_local_run):
             self.POC_INBOUND = 'H:/python'
-            self.fileName = 'sample.json'
+            self.fileName = 'BigFile.json'
             self.filePath = self.POC_INBOUND + self.fileConcatinator + self.fileName
             self.logFileName = "logFile_" + timestampStr + "." + "txt"
             self.logFilePath = self.POC_INBOUND + self.fileConcatinator + self.logFileName
@@ -103,13 +111,13 @@ class DataJsonFlatten:
         else:  
             print("")
             #SingleLineComment
-            POC_INBOUND = 'lly-future-state-arch-poc-dev' + self.fileConcatinator + 'input_data'
-            self.fileName = 'Source_file.json' 
+            POC_INBOUND = self.bucket_name + self.fileConcatinator + 'input_data'
+            self.fileName = 'BigFile.json' 
             self.filePath =  POC_INBOUND + self.fileConcatinator +  self.fileName  
             self.logFileName = "logFile_" + timestampStr + "." + "txt"
-            self.logFilePath = "input_data/" + self.fileConcatinator + self.logFileName
+            self.logFilePath = "logs" + self.fileConcatinator + self.logFileName
             self.outputFileName = "data_de_profiled_simplefied_json" + "." + self.output_type
-            self.outputFilePath = "input_data/" + self.fileConcatinator +  self.outputFileName
+            self.outputFilePath = "Data" + self.fileConcatinator +  self.outputFileName
             #SingleLineComment
 
     def getFile(self):
@@ -123,7 +131,7 @@ class DataJsonFlatten:
             #SingleLineComment
             s3=boto3.client('s3')  
             inputFolder = 'input_data'
-            self.file = s3.get_object(Bucket= 'lly-future-state-arch-poc-dev', Key= inputFolder + self.fileConcatinator + self.fileName)
+            self.file = s3.get_object(Bucket= self.bucket_name, Key= inputFolder + self.fileConcatinator + self.fileName)
             return self.file
 
             #SingleLineComment
@@ -256,7 +264,7 @@ class DataJsonFlatten:
                     if(key == 'Address' or key == 'Specialities'):
                         customObjValue = self.recursiveParser( valueForKey, valueModels, key, valueForKey.nestedLevel, 'label')
                         combinedValueForKey = self.combineMultipleInfo(customObjValue, combinedValueForKey)
-                        finalData['Address']= combinedValueForKey
+                        finalData[key]= combinedValueForKey
                         finalJsonHasKeys = combinedValueForKey and len(combinedValueForKey) > 0
                         continue
 
@@ -330,69 +338,99 @@ class DataJsonFlatten:
             pass 
 
     def writetoFile(self, output_string, filePath, fileName): 
-        print(fileName, filePath)
+        if(None == output_string or len(output_string) == 0):
+            return
+
         if(self.is_local_run): 
             with open(filePath,'a') as outfile:
                 outfile.write(output_string) 
-        else: 
-            print("")
+        else:  
             #SingleLineComment"
             client = boto3.client('s3') 
-            client.put_object(Body=output_string, Bucket='lly-future-state-arch-poc-dev', Key=  fileName)  
+            client.put_object(Body=output_string, Bucket=self.bucket_name, Key=  fileName)  
             #SingleLineComment
     
     def runTransformation(self, new_lines):
         try: 
-            #Loading Data to Memory as Json
+            #Loading Data to Memory as Json 
             allData = json.loads(new_lines)  
-            
+            self.initVariables()
+
             #Application Main
             # Load All Data and for eachData Run the Function
-            for entity in allData: 
-                print("\n Running Entity")
-                self.parseEntity(entity)  
+
+            if(self.read_line_byLine):
+                self.parseEntity(allData)  
+            else:
+                for entity in allData:  
+                    self.parseEntity(entity)   
+        
+            output_string = self.getOutputAsStringLineSeperated(self.output_data, True)  
             
-            print("\n Getting OutputString")
-            output_string = self.getOutputAsStringLineSeperated(self.output_data, True) 
-            print("\n Got OutputString")
-            self.writetoFile(output_string, self.outputFilePath, "Data/" + self.outputFileName) 
+            if(self.is_local_run):
+                self.writetoFile(output_string, self.outputFilePath,  self.outputFilePath) 
+            else:
+                self.final_output_string_for_s3 = self.final_output_string_for_s3 + output_string 
+             
+        finally:  
+            log_output_string = self.getOutputAsStringLineSeperated(self.logData, False) 
+            if(self.is_local_run):
+                self.writetoFile(log_output_string,  self.logFilePath, self.logFilePath) 
+            else:
+                self.final_log_string_for_s3 = self.final_log_string_for_s3 + log_output_string 
             
-            print("\n Writing completed")
-            
-            # Closing file 
-            if(self.fileVal and self.fileVal is not None):
-                self.fileVal.close()
- 
-            if(self.outLogfile and self.outLogfile is not None):
-                self.outLogfile.close() 
-        finally: 
-            print("\n Getting Log File String")
-            log_output_string = self.getOutputAsStringLineSeperated(self.logData, False)
-            print("\n Wrote Output Log completed")
-            self.writetoFile(log_output_string,  self.logFilePath, "logs/"+self.logFileName) 
 
     def main(self):
         try:
             self.setFileInputDirectory()
             
+            self.log('Deleting Existing Output File')
             #Delete File If Exists
             self.deleteFileIfExists(self.logFilePath)
             self.deleteFileIfExists(self.outputFilePath)  
+            self.log('Deleting Existing Output File Completed')
 
-            file = self.getFile()
+            self.log('Is this Local Run = ' + str(self.is_local_run))
+            self.log('Is Read Line By Line = ' + str(self.read_line_byLine)) 
+
+            file = self.getFile() 
+            self.log('Completed Get File') 
+            s3=boto3.client('s3')  
 
             if(self.read_line_byLine):
                 if(self.is_local_run):
                     lines = self.fileVal.readlines()
-                    for line in lines:
-                        self.runTransformation(line)
+                    i = -1
+                    for line in lines: 
+                        i = i + 1
+                        self.log('Iterating over line')
+                        self.log(str(i))
+                        if(line and len(line) > 10):
+                            line = line[:line.rindex('}') + 1]
+                            self.runTransformation(line)
 
                 else:
                    for i,line in enumerate(file['Body'].iter_lines()):
-                    line1 = line.decode('utf-8')
-                    self.log("Running for one line:")
-                    print(str(i))
-                    self.runTransformation(line1)
+                        line1 = line.decode('utf-8')  
+                        self.log('Iterating over line')
+                        self.log(str(i))
+                        if(i % 100 == 0):
+                            outputFileExistingFile = s3.get_object(Bucket= self.bucket_name, Key= self.outputFilePath)
+                            outputFileExistingData = outputFileExistingFile['Body'].read().decode('UTF-8')  
+                            self.final_output_string_for_s3 = outputFileExistingData + self.final_output_string_for_s3
+                            self.writetoFile(self.final_output_string_for_s3, self.outputFilePath,  self.outputFilePath) 
+                            self.final_output_string_for_s3 = ''
+
+                            logFileExistingFile = s3.get_object(Bucket= self.bucket_name, Key= self.logFilePath)
+                            logFileExistingData = logFileExistingFile['Body'].read().decode('UTF-8')  
+                            self.final_log_string_for_s3 = logFileExistingData + self.final_log_string_for_s3
+                            self.writetoFile(self.final_log_string_for_s3, self.logFilePath,  self.logFilePath) 
+                            self.final_log_string_for_s3 = '' 
+                         
+
+                        if(line1 and len(line1) > 10):
+                            line1 = line1[:line1.rindex('}') + 1]
+                            self.runTransformation(line1)
                     
             else:
                 if(self.is_local_run):
@@ -404,12 +442,20 @@ class DataJsonFlatten:
                     lines = "[" + lines + "]"
                     self.runTransformation(lines)
 
+            # Closing file 
+            if(self.fileVal and self.fileVal is not None):
+                self.fileVal.close()
+
+            if(self.outLogfile and self.outLogfile is not None):
+                self.outLogfile.close()
 
         finally: 
-            print("\n Getting Log File String")
-             
-
-
+            if(self.is_local_run):
+                print('End Of Program')
+            else:
+                self.writetoFile(self.final_output_string_for_s3, self.outputFilePath,  self.outputFilePath) 
+                self.writetoFile(self.final_log_string_for_s3,  self.logFilePath, self.logFilePath) 
+              
 if __name__ == "__main__":
     x = DataJsonFlatten()
     x.main()
